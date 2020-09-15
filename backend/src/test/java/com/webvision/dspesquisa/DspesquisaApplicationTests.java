ed17b6c2d634bc9960c317cad95726f9e8840fb8
Wellington Mazoni <wmazoni@protonmail.com>
2020-09-15 05:32:35 -0300

    Created Entities

--- backend/src/test/java/com/webvision/dspesquisa/DspesquisaApplicationTests.java
+++ backend/src/test/java/com/webvision/dspesquisa/DspesquisaApplicationTests.java
@@ -3,6 +3,7 @@ package com.webvision.dspesquisa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class DspesquisaApplicationTests {
 
