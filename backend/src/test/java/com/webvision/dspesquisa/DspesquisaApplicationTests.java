d8afe48f881ce8ad4742189cda296c3a997ebf52
Wellington Mazoni <wmazoni@protonmail.com>
2020-09-14 22:35:48 -0300

    Project created

---
+++ backend/src/test/java/com/webvision/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webvision.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
