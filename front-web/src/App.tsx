6ef9691af75e9b66fd6a3fba55ce18f4eae3c0ec
Wellington Mazoni <wmazoni@protonmail.com>
2020-09-16 20:57:02 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Index';
+
 
 function App() {
   return (
-    <h1>Hello semana Devsuperior!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
